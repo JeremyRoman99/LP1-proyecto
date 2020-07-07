f3210fd3e7a0b8345b083d331a4968294f5d7e8d
Jeremy Roman <jeremyjor299@gmail.com>
2020-07-07 14:10:31 -0500

    rr

---
+++ src/modularidad/Prog007.java
@@ -0,0 +1,24 @@
+
+package modularidad;
+
+
+public class Prog007 {
+    //metodo vacio
+    // vacio
+    public static void saludo(){
+        System.out.println("bienvenido al sistema");
+        
+            
+        }
+public static void despedida(){
+    System.out.println("gracias por usar este programa");
+}  
+public static mensaje();{
+            System.out.println("en todo tiempo ama el amigo.. prov. 17:17");
+}
+    
+    public static void main(String[] args) {
+        saludo();
+    }
+    
+}
