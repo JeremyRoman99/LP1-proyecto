43015b618cff51bcc34750cc7e47bf382d031d71
Jeremy Roman <jeremyjor299@gmail.com>
2020-07-07 14:34:12 -0500

    prog08 metodoque recibe parameto

--- src/modularidad/Prog008.java
+++ src/modularidad/Prog008.java
@@ -10,5 +10,24 @@ package modularidad;
  * @author JEREMY
  */
 public class Prog008 {
+    public static void saludo(String nombre){       
+        System.out.println("bienvenido" + nombre);
+        System.out.println("disfruta tu estadia en este lugar");              
+        }
+     public static void saludo2(String nombre,String carrera){
+        System.out.println("disfruta tu estadia en la carrera de" + carrera);              
+        }
+     public static void factorial(int num){        
+        int fac = 1;
+        for(int i=1; i<= num; i++){
+            fac*=i;
+        }
+        System.out.print("factorial de:" +num + " es" +fac);
+        }
+     public static void main(String[] args){
+        saludo("daniel");
+        saludo2("juan","ing.sistemas");
+        factorial(5);
+        }
     
 }
